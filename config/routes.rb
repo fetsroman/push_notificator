c752d51a125683d5160ca07c5f0aa4762b94999c
fetsroman <fets.roman@gmail.com>
2020-09-22 21:18:57 +0300

    Add Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   namespace :api do
     post '/sign_up', to: 'users#create'
     post '/sign_in', to: 'authentication#sign_in'
