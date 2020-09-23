f97066f94e337941d6054c52b8f8592f8654e225
fetsroman <fets.roman@gmail.com>
2020-09-23 17:43:29 +0300

    Add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   has_many :devices, dependent: :destroy
   has_many :notes, dependent: :destroy
+
+  validates_presence_of :email, :password
+  validates_uniqueness_of :email
 end
